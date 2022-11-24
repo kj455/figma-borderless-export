13da21242979b749eca261829c92ed75e28b999e
kj455 <kaji.ibuki45@gmail.com>
2022-11-24 22:14:32 +0900

    refactor: tsconfig

--- plugin/code.ts
+++ plugin/code.ts
@@ -6,60 +6,90 @@ type Asset = {
   height: number;
 };
 
-type Extension = 'png' | 'jpg';
-const exportSettingMap: Record<Extension, ExportSettingsImage[]> = {
-  png: [
-    {
+type Extension = typeof EXTENSION_LIST[number];
+type Scale = typeof SCALE_LIST[number];
+
+const EXTENSION_LIST = ['png', 'jpg'] as const;
+const SCALE_LIST = ['1x', '1.5x', '2x'] as const;
+const exportSettingMap: {
+  [key in Extension]: {
+    [key in Scale]: ExportSettingsImage;
+  };
+} = {
+  png: {
+    '1x': {
       format: 'PNG',
       suffix: '',
       constraint: { type: 'SCALE', value: 1 },
       contentsOnly: true,
     },
-    {
+    '1.5x': {
       format: 'PNG',
       suffix: '@1.5x',
       constraint: { type: 'SCALE', value: 1.5 },
       contentsOnly: true,
     },
-    {
+    '2x': {
       format: 'PNG',
       suffix: '@2x',
       constraint: { type: 'SCALE', value: 2 },
       contentsOnly: true,
     },
-  ],
-  jpg: [
-    {
+  },
+  jpg: {
+    '1x': {
       format: 'JPG',
       suffix: '',
       constraint: { type: 'SCALE', value: 1 },
       contentsOnly: true,
     },
-    {
+    '1.5x': {
       format: 'JPG',
       suffix: '@1.5x',
       constraint: { type: 'SCALE', value: 1.5 },
       contentsOnly: true,
     },
-    {
+    '2x': {
       format: 'JPG',
       suffix: '@2x',
       constraint: { type: 'SCALE', value: 2 },
       contentsOnly: true,
     },
-  ],
+  },
 };
 
 const formatName = (name: string) =>
   name.replace(/\s/g, '').split('/').pop()?.toString() || 'anonymous';
+
+const parseCommand = (
+  command: string
+): { ext: Extension; scaleList: Scale[] } => {
+  const [ext, scaleListStr] = command.split(':');
+  if (!EXTENSION_LIST.includes(ext as Extension)) {
+    throw new Error(`Could not parse command. extension: ${ext}`);
+  }
+
+  const scaleList = scaleListStr.split(',');
+  if (
+    scaleList.length === 0 ||
+    scaleList.some((s) => !SCALE_LIST.includes(s as Scale))
+  ) {
+    figma.closePlugin(`Could not parse command. scale: ${scaleListStr}`);
+  }
+
+  return { ext: ext as Extension, scaleList: scaleList as Scale[] };
+};
 
 const main = async (command: string) => {
   const { selection } = figma.currentPage;
   if (selection.length > 0) {
     figma.showUI(__html__, { visible: false });
 
+    const { ext, scaleList } = parseCommand(command);
+    const settings = scaleList.map((scale) => exportSettingMap[ext][scale]);
+
     const assets: Asset[] = await Promise.all(
-      exportSettingMap[command as Extension].flatMap((setting) => {
+      settings.flatMap((setting) => {
         return selection.map(async (selection) => {
           return {
             name: formatName(selection.name),
--- ui/types.ts
+++ ui/types.ts
@@ -1,3 +1,21 @@
+/**
+ * extract necessary types from @figma
+ */
+type ExportSettingsImage = {
+  readonly format: 'JPG' | 'PNG';
+  readonly contentsOnly?: boolean; // defaults to true
+  readonly useAbsoluteBounds?: boolean; // defaults to false
+  readonly suffix?: string;
+  readonly constraint?: ExportSettingsConstraints;
+};
+type ExportSettingsConstraints = {
+  readonly type: 'SCALE' | 'WIDTH' | 'HEIGHT';
+  readonly value: number;
+};
+
+/**
+ * Original types below
+ */
 export type Asset = {
   name: string;
   setting: ExportSettingsImage;
